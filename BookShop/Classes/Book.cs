b7505ee96a1d85c0fa2158cdf7698508573020e5
nastiandru <nastia.andr070703gmail.com>
2022-06-17 04:05:11 +0300

    Update Book.cs

--- BookShop/Classes/Book.cs
+++ BookShop/Classes/Book.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace BookShop.Classes
 {
-    internal class Book
+    public class Book
     {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public int timePeriod { get; set; }
+        public string author { get; set; }
+        public string edition { get; set; }
+        public string Summary { get; set; }
     }
 }
