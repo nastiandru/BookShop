fc7ce6490dcd9cb7d4037b19103d693a53fcfeb7
nastiandru <nastia.andr070703gmail.com>
2022-06-17 04:05:07 +0300

    Update Inventory.cs

--- BookShop/Classes/Inventory.cs
+++ BookShop/Classes/Inventory.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Serialization;
 
 namespace BookShop.Classes
 {
     internal class Inventory
     {
+        public static void WriteXML<T>(T books, string fileName)
+        {
+            //throw new NotImplementedException();
+            XmlSerializer srz = new XmlSerializer(typeof(T));
+            FileStream stream;
+
+
+
+            stream = new FileStream(fileName, FileMode.Create);
+            srz.Serialize(stream, books);
+            stream.Close();
+        }
+
+        internal static T ReadXML<T>(string fileName)
+        {
+            //throw new NotImplementedException();
+            XmlSerializer srz = new XmlSerializer(typeof(T));
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                    return (T)srz.Deserialize(sr);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+                return default(T);
+            }
+        }
     }
 }
